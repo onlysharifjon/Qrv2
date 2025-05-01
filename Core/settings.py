ae3fefc12ca44a0b0f2abbed0f2ebb40f1692378
onlysharifjon <mominovsharif12@gmail.com>
2025-05-02 00:18:36 +0500

    fix server error

--- Core/settings.py
+++ Core/settings.py
@@ -124,3 +124,5 @@ import os
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
