50f6d8372517dc78de51c75f37f903e163407b7b
bekiake <behruzatoyev00@gmail.com>
2025-05-03 01:40:38 +0500

    a

--- qrcodeapp/urls.py
+++ qrcodeapp/urls.py
@@ -4,6 +4,5 @@ from .views import ImageCreateAPIView,AddPDFAPIView,IDCardViewSet,QRScanView
 urlpatterns = [
     path('register/', ImageCreateAPIView.as_view(), name='register api'),
     path('upload/pdf/', AddPDFAPIView.as_view(), name='add pdf'),
-    path('idcardview',IDCardViewSet.as_view(), name='idcard view'),
-    path('qrscan',QRScanView.as_view(), name='QRScan'),
+    path('api/qr/', QRScanView.as_view(), name='qr-scan'),
 ]
