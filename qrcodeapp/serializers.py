c0a51f7e202bf89067a7529b66b491740b5cbd72
bekiake <behruzatoyev00@gmail.com>
2025-05-24 03:55:52 +0500

    html

--- qrcodeapp/serializers.py
+++ qrcodeapp/serializers.py
@@ -4,7 +4,7 @@ from .models import IDCard
 class IDCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = IDCard
-        fields = ['id', 'uuid', 'first_name', 'surname', 'birthday',
+        fields = ['id', 'uuid', 'first_name', 'gender', 'birthday',
                   'id_pass', 'country', 'blood_type', 'id_badge', 'user_image',
                   'qr_image', 'pdf_file', 'created_at']
         read_only_fields = ['uuid', 'qr_image', 'pdf_file', 'created_at']
